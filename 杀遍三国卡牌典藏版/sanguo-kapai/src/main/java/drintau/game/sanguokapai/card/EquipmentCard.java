18986914cdb4948147ab53daabb56749077a00c0
Drintau <1963704754@qq.com>
2026-08-02 20:29:00 +0800

    [杀遍三国卡牌典藏版](feat):一步一步实现游戏

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/card/EquipmentCard.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/card/EquipmentCard.java
@@ -1,8 +1,28 @@
 package drintau.game.sanguokapai.card;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 装备卡
  */
+@Getter
+@Setter
 public class EquipmentCard extends AbstractCard {
 
+    private String name;
+
+    private CardConstants.UnitType unitType;
+
+    private int addSpeed;
+
+    private int addAttack;
+
+    public EquipmentCard(String name, CardConstants.UnitType unitType, int addSpeed, int addAttack) {
+        setCardType(CardConstants.CardType.EQUIPMENT);
+        this.name = name;
+        this.unitType = unitType;
+        this.addSpeed = addSpeed;
+        this.addAttack = addAttack;
+    }
 }
---
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/EquipmentData.java
@@ -0,0 +1,10 @@
+package drintau.game.sanguokapai.data;
+
+import drintau.game.sanguokapai.card.CardConstants;
+import drintau.game.sanguokapai.card.EquipmentCard;
+
+public final class EquipmentData {
+
+    public static final EquipmentCard CHANG_QIANG = new EquipmentCard("长枪", CardConstants.UnitType.GUNNER, 0, 2);
+
+}
