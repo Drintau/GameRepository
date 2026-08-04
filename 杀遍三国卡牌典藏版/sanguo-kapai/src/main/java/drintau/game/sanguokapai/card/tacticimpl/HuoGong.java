f6fce54ea14cf3172a29996fd854f666aeed3037
Drintau <1963704754@qq.com>
2026-08-04 19:15:41 +0800

    [杀遍三国卡牌典藏版](feat):一步一步完善游戏

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/card/tacticimpl/HuoGong.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/card/tacticimpl/HuoGong.java
@@ -4,31 +4,41 @@ import drintau.game.sanguokapai.card.TacticCard;
 import drintau.game.sanguokapai.data.PlayerData;
 import drintau.game.sanguokapai.desktop.ActionItem;
 import drintau.game.sanguokapai.desktop.DesktopContext;
+import drintau.game.sanguokapai.util.DaemonScheduler;
 import drintau.game.sanguokapai.util.ThreadSleepUtil;
 import javafx.application.Platform;
 import javafx.scene.layout.StackPane;
+
+import java.util.concurrent.TimeUnit;
 
 public class HuoGong extends TacticCard {
 
     @Override
     public void exec(PlayerData playerData, int rowIndex) {
-        StackPane[][] cells = DesktopContext.getInstance().getCells();
-        for (int i = DesktopContext.peoplePlayerUnitInitColIndex; i <= DesktopContext.aiPlayerUnitInitColIndex; i++) {
-            StackPane cell = cells[rowIndex][i];
-            if (!cell.getChildren().isEmpty()) {
-                ActionItem targetCellActionItem = (ActionItem) cell.getUserData();
-                if (playerData.isAiFlag() != targetCellActionItem.isAiPlayer()) {
-                    if (targetCellActionItem.getUnitCard().getLevel() < 3) {
-                        targetCellActionItem.setDeadFlag(true);
-                        Platform.runLater(() -> {
-                            cell.getChildren().clear();
-                            cell.setUserData(null);
-                        });
-                        ThreadSleepUtil.sleepSeconds(1L);
+        DesktopContext.getInstance().getSelectCard().setDisable(true);
+        DaemonScheduler.getInstance().submitOnceDelayTask(() -> {
+            StackPane[][] cells = DesktopContext.getInstance().getCells();
+            for (int i = DesktopContext.peoplePlayerUnitInitColIndex; i <= DesktopContext.aiPlayerUnitInitColIndex; i++) {
+                StackPane cell = cells[rowIndex][i];
+                if (!cell.getChildren().isEmpty()) {
+                    ActionItem targetCellActionItem = (ActionItem) cell.getUserData();
+                    if (playerData.isAiFlag() != targetCellActionItem.isAiPlayer()) {
+                        if (targetCellActionItem.getUnitCard().getLevel() < 3) {
+                            targetCellActionItem.setDeadFlag(true);
+                            Platform.runLater(() -> {
+                                cell.getChildren().clear();
+                                cell.setUserData(null);
+                            });
+                            ThreadSleepUtil.sleepSeconds(1L);
+                        }
                     }
                 }
             }
-        }
+            ThreadSleepUtil.sleepSeconds(1L);
+            Platform.runLater(() -> {
+                DesktopContext.getInstance().getSelectCard().setDisable(false);
+            });
+        }, 1L, TimeUnit.SECONDS);
     }
 
     @Override
