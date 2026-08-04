aefd372f16913f1b22db2def20409cb51cd148f2
Drintau <1963704754@qq.com>
2026-08-04 22:38:47 +0800

    [杀遍三国卡牌典藏版](feat):一步一步完善游戏

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/TacticData.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/TacticData.java
@@ -3,6 +3,7 @@ package drintau.game.sanguokapai.data;
 import drintau.game.sanguokapai.card.TacticCard;
 import drintau.game.sanguokapai.card.tacticimpl.AnJianShangRen;
 import drintau.game.sanguokapai.card.tacticimpl.HuoGong;
+import drintau.game.sanguokapai.card.tacticimpl.LuoLei;
 import drintau.game.sanguokapai.card.tacticimpl.XiuYangShengXi;
 
 import java.util.List;
@@ -12,12 +13,14 @@ public final class TacticData {
     public static final TacticCard XIU_YANG_SHENG_XI = new XiuYangShengXi();
     public static final TacticCard AN_JIAN_SHANG_REN = new AnJianShangRen();
     public static final TacticCard HUO_GONG = new HuoGong();
+    public static final TacticCard LUO_LEI = new LuoLei();
 
     public static List<TacticCard> getAllTactics() {
         return List.of(
                 XIU_YANG_SHENG_XI,
                 AN_JIAN_SHANG_REN,
-                HUO_GONG
+                HUO_GONG,
+                LUO_LEI
         );
     }
 }
