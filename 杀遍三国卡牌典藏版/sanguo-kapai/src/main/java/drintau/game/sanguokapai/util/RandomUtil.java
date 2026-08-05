00aa31b2532070a79af32d68580849e5cb9a22f1
Drintau <1963704754@qq.com>
2026-08-05 19:51:11 +0800

    [杀遍三国卡牌典藏版](feat):一步一步完善游戏

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/util/RandomUtil.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/util/RandomUtil.java
@@ -23,7 +23,6 @@ public class RandomUtil {
 
     public static boolean roll(int successRate) {
         int randomInt = randomInt(rate100);
-        log.info("随机数是：{}", randomInt);
         return randomInt < successRate;
     }
 
@@ -32,7 +31,9 @@ public class RandomUtil {
      * 0 <= x < maxInt
      */
     public static int randomInt(int maxInt) {
-        return secureRandom.nextInt(maxInt);
+        int randomInt = secureRandom.nextInt(maxInt);
+        log.info("随机数是：{}", randomInt);
+        return randomInt;
     }
 
 }
