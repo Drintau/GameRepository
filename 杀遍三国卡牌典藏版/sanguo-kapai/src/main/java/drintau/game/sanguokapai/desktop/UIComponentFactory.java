94ab3cc780b1bcae2d8c84fb15f8fb310119a865
Drintau <1963704754@qq.com>
2026-08-11 18:51:12 +0800

    [杀遍三国卡牌典藏版](feat):细节优化

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/UIComponentFactory.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/UIComponentFactory.java
@@ -1,7 +1,16 @@
 package drintau.game.sanguokapai.desktop;
 
+import drintau.game.sanguokapai.card.TacticCard;
+import drintau.game.sanguokapai.data.PlayerData;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class UIComponentFactory {
@@ -12,6 +21,51 @@ public class UIComponentFactory {
         scrim.heightProperty().bind(root.heightProperty());
         scrim.setFill(Color.color(0, 0.5, 0, 0.2));
         return scrim;
+    }
+
+    public static BorderPane createExecTacticRoot(TacticCard tacticCard, int rowIndex, PlayerData playerData) {
+        BorderPane execTacticRoot = new BorderPane();
+        execTacticRoot.setPadding(new Insets(10));
+        execTacticRoot.setBackground(StyleConstants.WHITE_BACKGROUND);
+        execTacticRoot.setPrefWidth(300);
+        execTacticRoot.setPrefHeight(220);
+        execTacticRoot.setMinSize(300, 220);
+        execTacticRoot.setMaxSize(300, 220);
+
+        Label execTacticTitle;
+        if (playerData.isAiFlag()) {
+            execTacticTitle = new Label("电脑执行计策");
+        } else {
+            execTacticTitle = new Label("玩家执行计策");
+        }
+        execTacticTitle.setFont(StyleConstants.font24);
+        execTacticRoot.setTop(execTacticTitle);
+        BorderPane.setAlignment(execTacticTitle, Pos.CENTER);
+
+        Label execTacticCenter = new Label(tacticCard.getDescription());
+        execTacticCenter.setWrapText(true);
+        execTacticCenter.setFont(StyleConstants.font16);
+        execTacticRoot.setCenter(execTacticCenter);
+
+        HBox execTacticBottom = new HBox(10);
+        execTacticBottom.setAlignment(Pos.CENTER);
+        if (playerData.isAiFlag()) {
+            Label aiExecTacticLabel = new Label("请勿操作！电脑行动中");
+            aiExecTacticLabel.setFont(StyleConstants.font20);
+            execTacticBottom.getChildren().addAll(aiExecTacticLabel);
+            execTacticRoot.setBottom(execTacticBottom);
+        } else {
+            Button execTacticSureButton = new Button("确认");
+            execTacticSureButton.setFont(StyleConstants.font20);
+            execTacticSureButton.setOnAction(e -> {
+                tacticCard.exec(playerData, rowIndex);
+                DesktopContext.getInstance().getRoot().getChildren().remove(execTacticRoot);
+            });
+            execTacticBottom.getChildren().addAll(execTacticSureButton);
+        }
+        execTacticRoot.setBottom(execTacticBottom);
+
+        return execTacticRoot;
     }
 
 }
