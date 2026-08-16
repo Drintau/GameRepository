7a8459f603087f244edf3628545652f9384254aa
Drintau <1963704754@qq.com>
2026-08-16 13:29:15 +0800

    [杀遍三国卡牌典藏版v1.0.1](feat):首页界面

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/IndexScene.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/IndexScene.java
@@ -1,8 +1,12 @@
 package drintau.game.sanguokapai.desktop;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public final class IndexScene extends Scene {
 
@@ -13,6 +17,22 @@ public final class IndexScene extends Scene {
     private static Parent createContent() {
         BorderPane indexRoot = new BorderPane();
         indexRoot.setBackground(StyleConstants.BLUE_BACKGROUND);
+
+        Button startGame = new Button("开始游戏");
+        startGame.setFont(StyleConstants.font20);
+
+        Button showAllCard = new Button("卡牌一览");
+        showAllCard.setFont(StyleConstants.font20);
+
+        Button help = new Button("帮助说明");
+        help.setFont(StyleConstants.font20);
+
+        VBox indexCenter = new VBox(10);
+        indexCenter.setAlignment(Pos.CENTER);
+        indexCenter.setPadding(new Insets(10));
+        indexCenter.getChildren().addAll(startGame, showAllCard, help);
+
+        indexRoot.setCenter(indexCenter);
 
         return indexRoot;
     }
