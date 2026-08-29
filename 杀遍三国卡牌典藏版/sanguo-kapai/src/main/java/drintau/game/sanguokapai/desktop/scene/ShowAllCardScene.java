825cbd58615587401ed4b92d674ac64248433937
Drintau <1963704754@qq.com>
2026-08-29 11:23:17 +0800

    [杀遍三国卡牌典藏版v1.0.1](feat):界面完善

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/scene/ShowAllCardScene.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/scene/ShowAllCardScene.java
@@ -1,11 +1,20 @@
 package drintau.game.sanguokapai.desktop.scene;
 
+import drintau.game.sanguokapai.card.UnitCard;
+import drintau.game.sanguokapai.data.FormationFactory;
+import drintau.game.sanguokapai.data.HeroData;
+import drintau.game.sanguokapai.data.formation.AbstractFormation;
 import drintau.game.sanguokapai.desktop.DesktopContext;
 import drintau.game.sanguokapai.desktop.StyleConstants;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+import java.util.List;
 
 public class ShowAllCardScene extends Scene {
 
@@ -20,7 +29,22 @@ public class ShowAllCardScene extends Scene {
 
         showAllCardRoot.setTop(DesktopContext.getInstance().getShowIndexSceneBtn());
 
+        Pagination heroPagination = heroPagination();
+
+        showAllCardRoot.setCenter(heroPagination);
+
         return showAllCardRoot;
+    }
+
+    private static Pagination heroPagination() {
+        List<UnitCard> allHeroes = HeroData.getAllHeroes();
+        Pagination pagination = new Pagination(allHeroes.size(), 0);
+        pagination.setPageFactory(pageIndex -> {
+            Label label = new Label(allHeroes.get(pageIndex).getDescription());
+            label.setFont(StyleConstants.font24);
+            return new StackPane(label);
+        });
+        return pagination;
     }
 
 }
