149afc28d198ec6636c3af68ef3cb10bef897b0f
Drintau <1963704754@qq.com>
2026-08-29 12:34:17 +0800

    [杀遍三国卡牌典藏版v1.0.1](feat):界面完善

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/scene/PlayGameScene.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/desktop/scene/PlayGameScene.java
@@ -71,10 +71,13 @@ public final class PlayGameScene extends Scene {
         gameBoardPaneTop.setLeft(peoplePlayerInfo);
         gameBoardPaneTop.setCenter(turnCountLabel);
         gameBoardPaneTop.setRight(aiPlayerInfo);
+
+        BorderPane layoutPane = new BorderPane();
+        layoutPane.setLeft(desktopContext.getShowIndexSceneBtn());
         Label helpTextLabel = new Label("兵种克制关系：枪->骑->甲->术->盾->射->枪，器械无克制关系");
         helpTextLabel.setFont(StyleConstants.font24);
-        gameBoardPaneTop.setTop(helpTextLabel);
-        BorderPane.setAlignment(helpTextLabel, Pos.CENTER);
+        layoutPane.setCenter(helpTextLabel);
+        gameBoardPaneTop.setTop(layoutPane);
 
         StackPane[][] cells = new StackPane[DesktopContext.rows][DesktopContext.cols];
         desktopContext.setCells(cells);
