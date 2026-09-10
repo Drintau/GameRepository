399e480a9c1cd665da17422e4390382abc67c91d
Drintau <1963704754@qq.com>
2026-09-10 09:02:08 +0800

    [杀遍三国卡牌典藏版v1.0.2](feat):随机阵单位生成规则更新

--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/formation/AbstractFormation.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/formation/AbstractFormation.java
@@ -22,7 +22,6 @@ public abstract class AbstractFormation {
 
     private String name;
     private Integer unitCount;
-    private boolean randomFlag;
     private Map<CardConstants.UnitType, Integer> unitTypeCountMap =  new HashMap<>();
 
     // init是给单位、英雄列表填充实际数据
@@ -35,33 +34,22 @@ public abstract class AbstractFormation {
     }
 
     public AbstractFormation init(){
-        if (randomFlag) {
-            List<UnitCard> allUnitList = new ArrayList<>();
-            allUnitList.addAll(SoldierData.getAllSoldiers());
-            allUnitList.addAll(HeroData.getAllHeroes());
-            for (int i = 0; i < unitCount; i++) {
-                int randomIndex = RandomUtil.randomInt(allUnitList.size());
-                // 均存到士兵集合
-                soldierList.add(allUnitList.get(randomIndex));
+        // 士兵列表
+        Map<CardConstants.UnitType, List<UnitCard>> unitTypeCardListMap = SoldierData.getUnitTypeCardListMap();
+        for (Map.Entry<CardConstants.UnitType, Integer> unitTypeIntegerEntry : unitTypeCountMap.entrySet()) {
+            CardConstants.UnitType key = unitTypeIntegerEntry.getKey();
+            List<UnitCard> unitCardList = new ArrayList<>();
+            while (unitCardList.size() < unitTypeIntegerEntry.getValue()) {
+                unitCardList.addAll(unitTypeCardListMap.get(key));
             }
-        } else {
-            // 士兵列表
-            Map<CardConstants.UnitType, List<UnitCard>> unitTypeCardListMap = SoldierData.getUnitTypeCardListMap();
-            for (Map.Entry<CardConstants.UnitType, Integer> unitTypeIntegerEntry : unitTypeCountMap.entrySet()) {
-                CardConstants.UnitType key = unitTypeIntegerEntry.getKey();
-                List<UnitCard> unitCardList = new ArrayList<>();
-                while (unitCardList.size() < unitTypeIntegerEntry.getValue()) {
-                    unitCardList.addAll(unitTypeCardListMap.get(key));
-                }
-                soldierList.addAll(unitCardList);
-            }
+            soldierList.addAll(unitCardList);
+        }
 
-            // 英雄列表
-            List<UnitCard> allHeroList = HeroData.getAllHeroes();
-            for (int i = 0; i < unitCount - soldierList.size(); i++) {
-                int heroIndex = RandomUtil.randomInt(allHeroList.size());
-                heroList.add(allHeroList.get(heroIndex));
-            }
+        // 英雄列表
+        List<UnitCard> allHeroList = HeroData.getAllHeroes();
+        for (int i = 0; i < unitCount - soldierList.size(); i++) {
+            int heroIndex = RandomUtil.randomInt(allHeroList.size());
+            heroList.add(allHeroList.get(heroIndex));
         }
 
         return this;
--- 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/formation/SuiJiZhen.java
+++ 杀遍三国卡牌典藏版/sanguo-kapai/src/main/java/drintau/game/sanguokapai/data/formation/SuiJiZhen.java
@@ -1,10 +1,34 @@
 package drintau.game.sanguokapai.data.formation;
 
+import drintau.game.sanguokapai.card.UnitCard;
+import drintau.game.sanguokapai.data.HeroData;
+import drintau.game.sanguokapai.data.SoldierData;
+import drintau.game.sanguokapai.util.RandomUtil;
+
+import java.util.List;
+
 public final class SuiJiZhen extends AbstractFormation {
 
     public SuiJiZhen() {
         super(FormationConstants.SUI_JI_ZHEN, 50);
-        this.setRandomFlag(true);
+    }
+
+    @Override
+    public AbstractFormation init() {
+        List<UnitCard> allSoldiers = SoldierData.getAllSoldiers();
+        List<UnitCard> allHeroes = HeroData.getAllHeroes();
+
+        for (int i = 0; i < getUnitCount(); i++) {
+            // 英雄概率20%
+            if (RandomUtil.roll(RandomUtil.rate20)) {
+                int heroIndex = RandomUtil.randomInt(allHeroes.size());
+                getHeroList().add(allHeroes.get(heroIndex));
+            } else {
+                int soldierIndex = RandomUtil.randomInt(allSoldiers.size());
+                getSoldierList().add(allSoldiers.get(soldierIndex));
+            }
+        }
+        return this;
     }
 
     @Override
